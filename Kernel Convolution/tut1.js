750e14abc3d8296f134cf9c481639f128d850225
claudiga <claudeclodi@hotmail.co.uk>
2017-03-01 19:53:39 +0000

    made some changes

--- Kernel Convolution/tut1.js
+++ Kernel Convolution/tut1.js
@@ -1,9 +1,9 @@
 var img2 = new Image();
-img2.src = 'square.png';
+img2.src = 'psquare.png';
 
 img = new Image();
-img.src = 'bart.png';
-
+img.src = 'bart.jpg';
+img.style.display = 'none'
 
 
 
@@ -12,48 +12,184 @@ document.body.appendChild(img);
 
 var getpos = function(canvas){
     
-    canvas.addEventListener('click', function(e) {  
+    canvas.addEventListener('mousemove', function(e) {  
 
     var rect = canvas.getBoundingClientRect(); 
     var x = e.clientX - rect.left;              
-    var y = e.clientY - rect.top;               
+    var y = e.clientY - rect.top;  
+        
+        
+  
 
-    alert('Mouse position: ' + x + ',' + y);
+return [x,y];    
     
+        
+        
+        
+        
+        
 });
     
     var imdat,ctx; 
     
 }
 
+
+var canvas ; 
 img.onload = function(){
-var canvas =  document.getElementById('canvas1');
+canvas =  document.getElementById('canvas1');
  ctx = canvas.getContext('2d');
     canvas.height =img.height;
     canvas.width =img.width;
 ctx.drawImage(img,0,0);
-    getpos(canvas);
-    
- imdat = ctx.getImageData(0,0,314,472);
 
-   
-   
- 
+drawsquare(canvas);
 
 }
-var matrix = [];
-function setup(){
-  var pixel = 0;
-  
-for(var i=0; i<img.width *2; i++) {
+
+
+var getImageData = function(x,y,w,h){
+    
+  var  canvas =  document.getElementById('canvas1');
+           
+
+ var ctx = canvas.getContext('2d');
+    
+var  imdat = ctx.getImageData(x,y,w,h);
+   
+    imdat = imdat.data;
+    
+
+
+    var matrix = []; 
+    var pixel = 0;
+    
+
+/**for(var i=0; i<img.width *2; i++) {
     matrix[i] = [];
     for(var j=0; j<img.height*2; j++) {
         matrix[i][j] = imdat[pixel];
-        
         pixel++;
     }
+}
+ 
+ */
+ return imdat;
+}
+
+
+var drawsquare = function(canvas){
+    var drawn = false;
+    
+    var posx, posy; 
+    
+    
+
+    
+  //  window.alert(matrix)
+     canvas.addEventListener('mousemove', function(e) {  
+
+    var rect = canvas.getBoundingClientRect(); 
+    posx = e.clientX - rect.left;              
+    posy = e.clientY - rect.top;  
+    
+        //ctx.drawImage(img2,posx,posy)
+
+         
+  console.log('posx' + posx + ' posy' + posy);
+        
+         
+       
+// 199 - 8   
+         
+         var posxx = posx/16
+         
+         
+         
+         if(!drawn ){
+             
+           
+
+         ctx.drawImage(img2,Math.floor(posx/16) * 16  ,Math.floor(posy/16) * 16);
+             
+             drawn = true;
+         }else{
+             
+            
+        ctx.drawImage(img,0,0);
+             
+             xax = Math.floor(posx/16) * 16;  
+             yax = Math.floor(posy/16) * 16;
+             
+          //   
+        //     ctx.fillText('hello',xax,yax+10);
+             
+        var matrixx = getImageData(xax,yax,3,1);
+
+             console.log(yax );
+            red = matrixx[0];
+             green = matrixx[1];
+            
+            blue = matrixx[2];
+             
+         //   ctx.fillStyle = "#FF0000";
+
+             
+         ctx.fillStyle = 'rgb('+ red.toString()+ ','+green.toString()+',' + blue.toString() + ')';
+                  ctx.fillRect(xax,yax,16,16);
+
+             ctx.drawImage(img2,xax,yax);
+    console.log(red + ' ' + green + ' ' + blue);
+            
+         //   window.alert('rgba('+ red.toString()+ ','+green.toString()+',' + blue.toString() + ')');
+            
+            
+            
+            
+         
+         }
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+    
+    
+  
+
+     
+     
+ 
+
+
+         
+         });
+     
+    
+    
     
 }
+
+
+function setup(){
+
+    
+ 
   
    
 
@@ -62,16 +198,7 @@ for(var i=0; i<img.width *2; i++) {
 function draw(){
     
      
- for(var x = 0; x < img.width * 2; x+=14){
-     
-     for(var y = 0; y < img.height * 2; y+=14){
-         
-         ctx.drawImage(img2,x,y)
-         
-     }
-     
-     
- }
+ 
     
     
 }
